37de3a8ea2279b02cc3ed6c48f6be00f3b87e620
Kayden Clark <kaydenclark725@gmail.com>
2020-01-19 01:36:06 -0700

    started redive into python

---
+++ starting.py
@@ -0,0 +1,26 @@
+# Learning print
+
+print("Kayden Clark")
+print('o----')
+print(' ||||')
+print('*' * 10)
+
+# Learning Varibles
+
+price = 10 #int
+rating = 4.9 #float
+name = 'Kayden' #str
+is_published = True #bool
+print(price)
+
+# Learing how to store varibles
+is_new = True
+age = 20
+name = "John Smith"
+john_smith = name, is_new, age
+print(john_smith)
+
+# Learning concatination
+
+name = [input('What is your name? '), input('What is your favorite color? ')]
+print('Hello ' + name[0] + ', I also Like the color ' + name[1])
